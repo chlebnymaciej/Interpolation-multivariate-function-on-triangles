92e7aaeb1c46aab554e9e0f0ce8cb88729fa7d35
Maciej Chlebny <chlebny59@gmail.com>
2019-11-12 23:53:59 +0100

    version ready for commenting

--- InterpolationByTriangle.m
+++ InterpolationByTriangle.m
@@ -7,6 +7,7 @@ function [interpolatedValue] = ...
      
 dataToSum = [f0, f1, f2, f3, f4, f5].*[ksi0, ksi1, ksi2, ksi3, ksi4, ksi5];
 
-interpolatedValue = sum(dataToSum);
+% interpolatedValue = sum(dataToSum, 2);
+interpolatedValue = f0.*ksi0+f1.*ksi1+f2.*ksi2+f3.*ksi3+f4.*ksi4+f5.*ksi5;
 end
 
